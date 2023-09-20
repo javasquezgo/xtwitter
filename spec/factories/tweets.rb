93503a10120ab327ccd506604cc3144227c61a46
Jose Vasquez <jvasquez@ioogo.com>
2023-09-20 14:54:37 -0600

    Checked

--- spec/factories/tweets.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :tweet do
-    body { Faker::Lorem.characters(number: 255)}
-    user { create(:user)}
-    status { "created" }
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    username { Faker::Internet.username }
-  end
-end
