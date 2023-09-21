a08763f883a7cbc8465db42b11a392c1c230f9c6
Jose Vasquez <jvasquez@ioogo.com>
2023-09-21 13:36:51 -0600

    Validations were added to Tweet and User working in other validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ApplicationRecord
-    belongs_to :user, foreign_key: 'id_user'
+  belongs_to :user, foreign_key: 'id_user'
   has_many :bookmarks, foreign_key: 'id_tweet'
   has_many :likes, foreign_key: 'id_tweet'
+
+  validates :content, length: { maximum:256}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_many :followers, foreign_key: 'following_id', class_name: 'Follower'
   has_many :following, foreign_key: 'follower_id', class_name: 'Follower'
 
+  validates :email_user, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum:12}, format: { with: with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@\/\*\-\+\_])[A-Za-z\d!@\/\*\-\+\_\/\^\[\!\@\#\$\%\^\&\*\(\)\,\.\?\"\:\{\}\|\<\>\]\+\$\/]{12,}\z/ }
+
 end
