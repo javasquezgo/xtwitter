9a39a4810f76f6439e911ad1551b97c302cd9db3
Jose Vasquez <jvasquez@ioogo.com>
2023-09-28 09:17:54 -0600

    Understanding routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,32 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :tweets, only: [:new, :create]
+
+  resources :tweets, only: [:edit, :update]
+
+  resources :tweet, only: [:new, :create, :update] do
+    member do
+      get :action
+    end
+  end
+
+
+
+  #All user routes
+  #get '/users', to: "users#index"
+  #New user route
+  #get '/users/new', to: "users#new"
+  #Creater user route
+  #post '/users', to: "users#create"
+  #Show user route  
+  #get '/users/:id', to: "users#show"
+  #Edit user route
+  #get '/users/:id/edit', to: "users#edit"
+  #Update user route
+  #put '/users/:id', to: "users#update"
+  #Delete user route
+  #delete '/users/:', to: "users#destroy"
+
 end
