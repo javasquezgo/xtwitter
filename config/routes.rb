30bba60388879c0f9e23b530b93552dfe0549762
Jose Vasquez <jvasquez@ioogo.com>
2023-10-02 10:29:39 -0600

    Fixing problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  #resources :users
-
-  resources :users, only: [] do
-    get 'tweets(/page/:page)', to: 'users#tweets'
-    get 'tweets_and_replies', to: 'users#tweets_and_replies'
+  resources :users do
+     get 'tweets(/page/:page)', to: 'users#tweets'
+     get 'tweets_and_replies', to: 'users#tweets_and_replies'
   end 
 
-
-  resources :users, only: [:index, :edit, :update, :show, :destory] do
-    
+  resources :users do
+    resources :likes, only: [:index]
   end
 
-  resources :tweets, only: [:new, :create, :update] do
+  resources :tweets, only: [:new, :index, :show, :create, :update] do
     member do
       post 'like', to: 'tweets#like'
       delete 'unlike', to: 'tweets#unlike'
@@ -23,22 +20,5 @@ Rails.application.routes.draw do
       post 'bookmark', to: 'tweets#bookmark'
     end
   end
-
-  #Show user route  
-  #get '/users/:id', to: "users#show"
-
-  #All user routes
-  #get '/users', to: "users#index"
-  #New user route
-  #get '/users/new', to: "users#new"
-  #Creater user route
-  #post '/users', to: "users#create"
-  
-  #Edit user route
-  #get '/users/:id/edit', to: "users#edit"
-  #Update user route
-  #put '/users/:id', to: "users#update"
-  #Delete user route
-  #delete '/users/:', to: "users#destroy"
 
 end
