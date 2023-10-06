5f52a06f893156715846b5f5876d58999a4e6537
Alejandro Vasquez <josejaguer12@gmail.com>
2023-10-05 19:52:43 -0600

    Before removing tables from User and were fixing issues

--- app/controllers/web/tweets_controller.rb
+++ app/controllers/web/tweets_controller.rb
@@ -1,7 +1,5 @@
 class Web::TweetsController < ApplicationController
 
-    before_action :authenticate_user!
-
     def index
         @tweets = Tweet.all
     end
--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -1,7 +1,5 @@
 class Web::UsersController < ApplicationController
 
-    before_action :authenticate_user!
-
     def index
         @users = User.all
     end
